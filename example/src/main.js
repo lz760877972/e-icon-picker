d78fa2ef567164d404dfa968fb45b5ad67c30b6e
李振 <760877972@qq.com>
2021-07-08 19:33:27 +0800

    version log

--- example/src/main.js
+++ example/src/main.js
@@ -26,7 +26,12 @@ app.use(eIconPicker, {
     removeIconList: [],
     zIndex: 3100
 });
-console.log(version);
+console.log(
+    `%c e-icon-picker %c v${version} %c`,
+    'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+    'background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
+    'background:transparent'
+)
 
 iconList.addIcon(svgIcons);
 iconList.removeIcon(["el-icon-s-ticket"]);
