1333958aac809fb2417350d3981ee9c530ccc6d7
李振 <760877972@qq.com>
2022-04-16 23:58:11 +0800

    ts 声明文件

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+import {EIconPicker, EIconPickerOptions} from "./e-icon-picker";
+import {EIcon} from "./e-icon";
+import Vue from "vue";
+
+export declare const version: string;
+
+export function install(vue: typeof Vue, options: EIconPickerOptions): void
+
+export {EIconPicker, EIcon}
+export declare const iconList: Object;
+export declare const elementUI: Array<String>;
+export declare const fontAwesome: Array<String>;
+export declare const eIconList: Array<String>;
+export declare const analyzingIconForIconfont: Function;
+export declare const eIconSymbol: Function;
